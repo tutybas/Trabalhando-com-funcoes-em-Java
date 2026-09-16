c72aa4bedbd54a48b4618da0750c622d61ab92a1
Tuty <arthur.almeida.oliveira.xavier@gmail.com>
2026-09-16 12:14:41 -0300

    add func fatorial

--- funçoes.java
+++ funçoes.java
@@ -8,6 +8,13 @@ public class funçoes{
         JOptionPane.showMessageDialog(null,"o dobro do numero é "+i);
     }
 
+    public static int fatorial (int a){
+        for(int i=a-1; i>=1; i--){
+           a *= i;
+        }
+        return a;
+    }
+
 
     public static void main(){
 
