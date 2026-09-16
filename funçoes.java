d1d623b8b59dc20304e7ddd092532f6585205420
Tuty <arthur.almeida.oliveira.xavier@gmail.com>
2026-09-16 12:13:04 -0300

    add func dobro

--- funçoes.java
+++ funçoes.java
@@ -2,8 +2,12 @@ package funçôes;
 import javax.swing.JOptionPane;
 
 public class funçoes{
-
     
+    public static void Dobro(int a){
+        int i = a*2; 
+        JOptionPane.showMessageDialog(null,"o dobro do numero é "+i);
+    }
+
 
     public static void main(){
 
