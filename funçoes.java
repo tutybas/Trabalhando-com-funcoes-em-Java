94a0f565839fa20f35786e2e3c4d3e577403da9a
Tuty <arthur.almeida.oliveira.xavier@gmail.com>
2026-09-16 16:01:52 -0300

    complementando o main

--- funçoes.java
+++ funçoes.java
@@ -1,4 +1,3 @@
-package funçôes;
 import javax.swing.JOptionPane;
 
 public class funçoes{
@@ -60,8 +59,88 @@ public class funçoes{
         return z;
 
     }
-    
-    public static void main(){
 
+    public static void main(String[] args){
+        boolean func = true;
+        while(func){
+            String opcao = JOptionPane.showInputDialog(null,"1.Cacular o dobro\n 2.Calcular o fatorial\n 3.Cacular a média\n 4.Verificar se é primo\n 5.Calcular produto\n 6.comparar tamanhos\n 7.Concerter para radianos\n 8.Sair\n\n\n");
+            try{
+                switch (opcao){
+                    case "1":
+                        int a;
+                        String x =JOptionPane.showInputDialog(null,"Digite um numero");
+                        a = Integer.parseInt(x);
+                        Dobro(a);      
+                    break;
+
+                    case "2":
+                        int a2,f2;
+                        String x2 =JOptionPane.showInputDialog(null,"Digite um numero");
+                        a2 = Integer.parseInt(x2);
+                        f2 = fatorial(a2);
+                        JOptionPane.showMessageDialog(null,"o fatorial é " + f2);
+                    break;
+
+                    case "3":
+                        int a3,b3,c3,d3;
+                        String x3 =JOptionPane.showInputDialog(null,"Digite a primeira nota");
+                        a3 = Integer.parseInt(x3);
+                        String y3 =JOptionPane.showInputDialog(null,"Digite a segunda nota");
+                        b3 = Integer.parseInt(y3);
+                        String w3 =JOptionPane.showInputDialog(null,"Digite a terceira nota");
+                        c3 = Integer.parseInt(w3);
+                        String z3 =JOptionPane.showInputDialog(null,"Digite a quarta nota");
+                        d3 = Integer.parseInt(z3);
+                        media(a3,b3,c3,d3);
+                    break;
+
+                    case "4":
+                        int a4;
+                        String x4 =JOptionPane.showInputDialog(null,"Digite um numero");
+                        a4 = Integer.parseInt(x4);
+                        primo(a4);
+                    break;
+
+                    case "5":
+                        int a5,b5,p5;
+                        String x5 =JOptionPane.showInputDialog(null,"Digite um numero");
+                        a5 = Integer.parseInt(x5);
+                        String y5 =JOptionPane.showInputDialog(null,"Digite outro numero");
+                        b5 = Integer.parseInt(y5);
+                        p5 = produto(a5,b5);
+                        JOptionPane.showMessageDialog(null,"O produto é " + p5);
+                    break;
+
+                    case "6":
+                        float a6,r6;
+                        String x6 =JOptionPane.showInputDialog(null,"Digite um grau");
+                        a6 = Float.parseFloat(x6);
+                        r6 = radianos(a6);
+                        JOptionPane.showMessageDialog(null,"O produto é " + r6);
+                    break;
+
+                    case "7":
+                        int a7,b7;
+                        String x7 =JOptionPane.showInputDialog(null,"Digite um numero");
+                        a7 = Integer.parseInt(x7);
+                        String y7 =JOptionPane.showInputDialog(null,"Digite outro numero");
+                        b7 = Integer.parseInt(y7);
+                        tamanho(a7,b7);
+                    break;
+                    
+                    case "8":
+                        func = false;
+                    break;
+                    
+                    default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida");
+
+                }
+            }
+            catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(null,"Por favor, digite um numero.","ERRO!!",2);
+            }
+                
+        }
     }
 }
